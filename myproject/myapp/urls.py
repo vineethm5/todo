b75100f29120040432ca235271f22fd86b5657ca
vineethm5 <1609567@sdmcujire.in>
2024-05-24 13:30:04 +0530

    Working succesfully

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns=[
-    path("",home,name='home')
+    path("",home,name='home'),
+    path("dele/<int:id>",dele,name='delete')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import *
+from django.contrib import messages
 
 
 def home(req):
-    return render(req,"home.html")
+    itemlist=todo.objects.order_by("-date")
+    if req.method == 'POST':
+        form=todoform(req.POST)
+        if form.is_valid:
+            form.save()
+            messages.info(req,"Note Added Successfully")
+            return redirect("/")
+        else:
+            form=todoform()
+    form=todoform()
+    page={
+        'title':"Title",
+        'forms':form,
+        'list':itemlist
+    }
+    return render(req,"home.html",page)
+
+
+def dele(req,id):
+    if req.method == "POST":
+        queryset=todo.objects.get(id=id)
+        queryset.delete()
+        messages.info(req,"Notes deleted Successfully")
+        return redirect("/")
+
+
 
 
 
